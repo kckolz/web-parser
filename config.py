98240374e4044aaeccca6175c77a871ece2a3edf
Kevin Kolz <kckolz@gmail.com>
2020-08-21 13:45:33 -0500

    scrubbing creds

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BASE_URL = "http://localhost:3000/v1"
-CLIENT_ID = "9359f577-bf8e-4c69-9dcc-45c721a538ae"
-CLIENT_SECRET = "060f08a9-e2ec-481d-a69f-1cf580b0e9db"
+BASE_URL = "https://api.whetstoneeducation.com/v1"
+CLIENT_ID = "YOUR_ID"
+CLIENT_SECRET = "YOUR_SECRET"
