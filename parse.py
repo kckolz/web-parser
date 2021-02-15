f4c6098fb74554c96da1300de637c0e2252b04d9
Kevin Kolz <kckolz@gmail.com>
2021-02-15 11:50:49 -0600

    saving json to file

--- parse.py
+++ parse.py
@@ -6,11 +6,15 @@ def parse_page(url):
     page = requests.get(url)
     return BeautifulSoup(page.content, 'html.parser')
 
+def save_json(file):
+    with open('page.json', 'w') as outfile:
+        json.dump(file, outfile)
+
 try:
     # instantiate API class
     parsedPage = parse_page('https://vergrabber.kingu.pl/vergrabber.json')
     newDictionary = json.loads(str(parsedPage))
-    print(newDictionary)
+    save_json(newDictionary)
 
 except Exception as e:
     print("Error: {0}".format(e))
